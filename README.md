82cb8144b97129b756eedb3a338967779a9edd91
foxton_ <51805025+f0xt0n@users.noreply.github.com>
2025-02-05 12:12:33 +1000

    release v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# tagoria-dailies
-Auto-completes daily tasks including quests, plunders, &amp; farming. Forever.
+<banner-img.jpg>
+> Thine earthmother comes upto thee, gifts abound.
+> Don thou life of earth, for a great many victory ye shall have.
+> — The Druid
+
+##### A daily task completer for the browser fantasy RPG, Tagoria.
+
+Auto-completes daily tasks including quests, plunders, & farming. Forever.
+
+FEATURES::
+- Quests - accepts, attempts, & turns-in quests by going to quest location and plundering.
+- Plundering - plunders chosen location when out of Quest Points with remaining Action Points.
+- Farming - farms for 24 hrs when out of Action Points until points reset at new day. Collects wages every 8 hrs.
+- Skiller (WIP) - increase attributes on levelup, buys skillpoints with extra amber above threshold.
+- Amber Preserver (WIP) - can set max amber carrying threshold if saving for weapons/armour/enchantments. Avoids carrying a large stealable sum by spending excess on skillpoints through Skiller.
+
+
+REQUIREMENTS::
+- Firefox
+- Geckodriver - put exe in same folder as 'TagoriaDailies.exe' - [link](https://github.com/mozilla/geckodriver/releases)
+
+
+INSTRUCTIONS::
+1. (Optional but Recommended to increase speed & reduce bandwidth) Download Firefox extensions 'uBlock' & 'NoScript' (right-click "Add to Firefox" -> "Save Link As"). Rename files to 'ublock_origin.xpi' & 'noscript.xpi' and put them in the same folder as 'TagoriaDailies.exe'.
+2. Enter credentials & edit preferences in 'config.yml'.
+3. Confirm character is not currently taking an action, and has turned-in any quest. Logout.
+4. Start 'TagoriaDailies.exe'.
+5. Sit back & relax (or leave it running on a raspberry pi and forget about it for months 😅)
+
+Notes:
+- If using extensions, the action timers won't show by default. Open 'NoScript' in top right corner, trust 'tagoria.net', untrust every other URL.
+- If you encounter an error - logout, close browser, check instructions, restart script.
+
+
+DEVELOPMENT::
+
+Code is far from ideal, and is not hardened or self-healing if an error occurs.
+
+Dependencies required for dev --
+- Firefox
+- Python 3
+- Selenium 4
+- Splinter 0.21
+
+
+Made with ❤️ by foxton_
---
+++ tagoria-dailies.py
@@ -0,0 +1,301 @@
+# Tagoria Dailies Completer
+
+import random
+from datetime import datetime, timedelta
+from time import sleep
+from splinter import Browser
+import yaml
+
+try:
+    browser = Browser('firefox', extensions=['ublock_origin.xpi', 'noscript.xpi'], capabilities={"acceptInsecureCerts": True})
+    print ('[✅] Using Firefox with extensions.')
+except:
+    browser = Browser('firefox', capabilities={"acceptInsecureCerts": True})
+    print ('[❌] Using Firefox without extensions.')
+
+# Config
+config = yaml.safe_load(open("config.yml"))
+USERNAME = config['USERNAME']
+PASSWORD = config['PASSWORD']
+PLUNDER_LOCATION = config['PLUNDER_LOCATION']
+AMBER_MAX = config['AMBER_MAX']
+
+# Initial variable setup
+days = 0
+new_day = True
+action_points = 6
+quest_points = 3
+stat_rotation = 1
+quest_location = '/mountains/overview/zone/?w=1IN&thiszone=5' # Default location to plunder is Canyon
+quest_complete = False
+
+
+# Login
+def login():
+    if browser.links.find_by_href('/auth/loginform/'):
+        print ('[*] Logging in..')
+        browser.find_by_id('menuLink1').first.click()
+        browser.find_by_name('world').select('1IN')
+        browser.fill('username', USERNAME)
+        browser.fill('password', PASSWORD)
+        browser.find_by_name('LABEL_LOGIN').click()
+        sleep(6)
+    else:
+        return
+    return
+
+
+# Collect Wages
+def wages():
+    if browser.is_element_present_by_id('leftNewsLink') and browser.is_element_present_by_xpath("//*[contains(@href,'/town/farm/?w=1IN')]"):
+        browser.find_by_id('leftNewsLink').click()
+        browser.find_by_name('ACTION_COLLECT_WAGE').click()
+        return print ('[*] Collected wage from farm.')       
+    else:
+        return print ('[*] No wage to collect.')
+
+
+# Work at Farm
+def farm():
+    global action_points
+    global quest_points
+    global new_day
+    global days
+
+    print ('[*] Going to Farm..')
+    browser.find_by_id('menuLink4').click() # Go to Village
+    sleep(3)
+    browser.find_by_id('menuLink14').click() # Go to Farm in Village
+    times_worked = 0
+    while times_worked != 3:
+        print ('[*] Working for 8 hours..')
+        browser.find_by_name('workHoursSelectList').select('8')
+        browser.find_by_name('ACTION_BEGIN_WORK').click()
+        sleep(random.randint(10,21))
+        print ('[*] Logging out..')
+        browser.find_by_id('menuLink10').click() # Logout
+        print ('[*] Zzz..')
+        wait_until(8,random.randint(3,6),random.randint(3,52))
+        print ("[*] Awaken! and claim what is yours..")
+        login()
+        wages()
+        times_worked += 1
+        print ('[*] Times worked: ' + str(times_worked))
+    days +=1
+    print ('[🌄] The sun rises once again. For ' + days + ' days, the legend continues..!')
+    new_day = True
+    action_points = 6
+    quest_points = 3
+    return
+
+
+# Quest checks
+def quest():
+    global action_points
+    global quest_points
+    global quest_location
+    global quest_complete  
+    global new_day
+
+    # Go to Druid
+    print ('[*] Going to Druid..')
+    browser.find_by_id('menuLink4').click() # Go to Village
+    sleep(3)
+    browser.find_by_id('menuLink11').click() # Go to Druid in Village
+    sleep(3)
+    browser.find_by_id('druid_mission').click()
+
+    # Turn in quest
+    if quest_complete:
+        if browser.is_element_present_by_xpath("//*[contains(@id,'btn_complete_')]"):
+            browser.find_by_xpath("//*[contains(@id,'btn_complete_')]").click()
+            print ('[*] Turned in Quest.')
+            sleep(3)
+            browser.find_by_id('druid_mission').click()
+            quest_complete = False
+        else:
+            print ('[*] No quest to turn in.')
+
+    # Check if already have quest
+    if browser.is_element_present_by_id('btn_abandon'):
+        print ('[*] Quest already in progress.')
+
+    # Accept new quest
+    # NOTE: accepts new quest before checking for AP so if we have any left over QP for the day, we can accept the quest for tomorrow and allow for them to effectively 'carry-over' to new day.
+    if browser.links.find_by_href('/town/druid/accept/?w=1IN'):
+        browser.links.find_by_href('/town/druid/accept/?w=1IN').click()
+        quest_points -= 1
+        quest_complete = False
+        print ('[*] Quest Points: ' + str(quest_points))
+
+    # Check if we have any points
+    if action_points == 0:
+        new_day = False
+        return print ('[*] No Action Points left, going to farm..')
+    elif quest_points == 0:
+        print ('[*] No Quest Points left, going to plunder..')
+
+    # Determine quest location
+    if browser.find_by_css('.mission_table'):
+        location = browser.find_by_css('.mission_table').last
+        if 'valley' in location.text:
+            print ('[*] Quest location is: Valley')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=1'
+        elif 'river' in location.text:
+            print ('[*] Quest location is: River')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=2'
+        elif 'ruins' in location.text:
+            print ('[*] Quest location is: Ruins')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=3'
+        elif 'mine' in location.text:
+            print ('[*] Quest location is: Mine')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=4'
+        elif 'canyon' in location.text:
+            print ('[*] Quest location is: Canyon')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=5'
+        elif 'volcano' in location.text:
+            print ('[*] Quest location is: Volcano')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=6'
+        else:
+            print ('[*] No quest active, going to Canyon..')
+            quest_location = '/mountains/overview/zone/?w=1IN&thiszone=5'
+    
+    plunder()
+    return
+
+
+# Begin Quest or Plunder Location until run out of Action Points
+def plunder():
+    global action_points
+    global quest_complete
+    global amber_threshold
+
+    browser.find_by_id('menuLink5').click() # Go to Mountains
+    browser.links.find_by_href(quest_location).click() # Go to Quest Location
+    quest_complete = False
+
+    while action_points != 0 and not quest_complete:
+        if browser.is_element_present_by_name('MISSION_BUTTON'):
+            action = 'MISSION_BUTTON'
+            print ('[*] Attempting Quest..')
+        elif browser.is_element_present_by_name('EXPLORATION_BUTTON'):
+            action = 'EXPLORATION_BUTTON'
+            print ('[*] Attempting Quest..')
+        elif browser.is_element_present_by_name('PLUNDER_BUTTON'):
+            action = 'PLUNDER_BUTTON'
+            print ('[*] Plundering..')
+
+
+        while not quest_complete: # Attempts quest until succeeds or runs out of Action Points                
+            if action_points == 0:
+                break
+            browser.find_by_name(action).click()
+            action_points -= 1
+            print ('[*] Current AP: ' + str(action_points))
+            print ('[*] Battling..')
+            wait_until(0,10,random.randint(25,46)) # Wait 10 mins 25-45 secs
+            if browser.is_text_present('Winner: ' + USERNAME):                
+                print ('[*] Victory!')
+            else:
+                print ('[*] Defeat!')    
+            if browser.is_text_present('Well done! You have accomplished your task.') or browser.is_text_present('You have successfully explored the region.'):                    
+                print ('[*] Quest Complete.')
+                print ('--------------------')
+                quest_complete = True
+                sleep(9)
+            else:
+                browser.links.find_by_href(quest_location).click()
+            # Check if levelled up
+            levelup()    
+    return
+
+
+# Check if levelled up
+def levelup():    
+    if browser.is_element_present_by_id('leftNewsLink') and browser.links.find_by_href('/char/attributes/levelup/?w=1IN'):
+        # browser.is_element_present_by_xpath("//*[contains(@href,'/town/levelup/?w=1IN')]")
+        print ('[*] Collecting levelup reward..')
+        browser.find_by_id('leftNewsLink').click()
+        #browser.find_by_name('ACTION_COLLECT_LEVELUP').click()
+        browser.links.find_by_href('/char/attributes/levelup/?w=1IN&1738627445&collect=1').click()
+    else:
+        return
+    return
+
+
+# Increase attributes in sequence
+def skiller():
+    global stat_rotation
+
+    browser.find_by_id('menuLink1').click() # Go to Character Stats
+
+    #Spend skillpoints on stats in rotation until depleted
+    while not browser.is_text_present('insufficient') == True:
+        if stat_rotation == 1:
+            browser.find_by_id('incr_str').click() # Increase Strength
+            sleep(3)
+            stat_rotation += 1
+        elif stat_rotation == 2:
+            browser.find_by_id('incr_dex').click() # Increase Dexterity
+            sleep(3)
+            stat_rotation += 1
+        elif stat_rotation == 3:
+            browser.find_by_id('incr_agi').click() # Increase Agility
+            sleep(3)
+            stat_rotation += 1
+        elif stat_rotation == 4:
+            browser.find_by_id('incr_sta').click() # Increase Stamina
+            sleep(3)
+            stat_rotation = 1
+        else:
+            stat_rotation = 1
+
+    #Buy skillpoint until no amber or have 8 skillpoints
+    while not browser.is_text_present('insufficient') == True:
+        browser.find_by_id('buy_SP').click() # Purchase Skillpoints
+    return
+
+
+# Wait
+def wait_until(hours,minutes,seconds):
+    future = datetime.replace(datetime.now() + timedelta(hours=hours,minutes=minutes,seconds=seconds))
+    print ('[*] Completion : ' + str(future))
+    while True:
+        diff = (future - datetime.now()).total_seconds()
+        # Increased precision
+        if diff <= 0:
+            return
+        elif diff <= 0.1:
+            sleep(0.001)
+        elif diff <= 0.5:
+            sleep(0.01)
+        elif diff <= 1.5:
+            sleep(0.1)
+        else:
+            sleep(1)
+
+
+# Error Handling - lol
+def error():
+    print ('[!!] Error [!!]')
+    sleep(9)
+    browser.find_by_id('menuLink10').click() # Logout
+    return
+
+
+
+# Entry
+print ('---~|| Initiate Tagoria Dailies Completer ||~---')
+browser.visit('https://www.tagoria.net/?lang=en')
+while True:
+    login()
+    wages()
+    while new_day:
+        quest()
+    farm()
+
+
+# Forseen Issues::
+# - Eventually the midnight points reset might intersect with when script is active doing quests/plundering. 
+# - This will desync points value stored in code and actual points, resulting in script not using up all points for the day.
+# - 1 day skip inefficiency :'(
